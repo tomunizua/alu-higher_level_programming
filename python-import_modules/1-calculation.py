8c8b70f270cb0fcfbaddd855cf260d189d5e985b
tomunizua <t.omunizua@alustudent.com>
2023-07-11 12:21:12 -0700

    Updated 1-calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as calculator
+import calculator_1 as calc
 a = 10
 b = 5
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, calculator.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator.div(a, b)))
+    print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc.div(a, b)))
