ec5065f22f2e0b100ee5cec5fcfcc9516d5bd547
tomunizua <t.omunizua@alustudent.com>
2023-07-11 12:24:00 -0700

    Refactored 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as calc
-a = 10
-b = 5
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, calc.add(a, b)))
-    print("{} - {} = {}".format(a, b, calc.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calc.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calc.div(a, b)))
+    import calculator_1 as calc_f
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, calc_f.add(a, b)))
+    print("{} - {} = {}".format(a, b, calc_f.sub(a, b)))
+    print("{} * {} = {}".format(a, b, calc_f.mul(a, b)))
+    print("{} / {} = {}".format(a, b, calc_f.div(a, b)))
