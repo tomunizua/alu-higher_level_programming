3ebde9c4c3ed8a47a3dd95c24b8d7e31c4d70d77
tomunizua <t.omunizua@alustudent.com>
2023-07-11 12:44:03 -0700

    Solved 100

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+import sys
+if __name__ == "__main__":
+    argv = sys.argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    if argv[2] not in ["+", "-", "*", "/"]:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    if argv[2] == "+":
+        result = add(int(argv[1]), int(argv[3]))
+    elif argv[2] == "-":
+        result = sub(int(argv[1]), int(argv[3]))
+    elif argv[2] == "*":
+        result = mul(int(argv[1]), int(argv[3]))
+    else:
+        result = div(int(argv[1]), int(argv[3]))
+    print(f"{int(argv[1]} {argv[2]} {int(argv[3])} = {result}")
