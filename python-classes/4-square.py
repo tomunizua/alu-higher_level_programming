831a9c9dee654f67654826fdee07caf6c175b350
tomunizua <t.omunizua@alustudent.com>
2023-07-26 13:54:39 -0700

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+This module is for python
+
+"""
+
+
+class Square:
+    """
+
+    This module is for python
+
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size * self.__size)
