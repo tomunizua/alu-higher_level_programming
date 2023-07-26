e78160d7c106033339e5537daf073641494dea67
tomunizua <t.omunizua@alustudent.com>
2023-07-26 13:18:59 -0700

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+This module is for python
+
+"""
+
+
+class Square:
+    """
+
+    This module is for python
+
+    """
+    def __init__(self, size):
+        self.__size = size
