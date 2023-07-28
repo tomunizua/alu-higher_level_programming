59afa13f727c1b1beb9928d00605b08130d90183
tomunizua <t.omunizua@alustudent.com>
2023-07-28 10:59:40 -0700

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Defines function is_same_class
+'''
+
+
+def is_same_class(obj, a_class):
+    '''Returns true if obj is an instance of type a_class
+    '''
+    return(type(obj) == a_class)
