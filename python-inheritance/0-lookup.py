d3e43410453d569d9700baa07c06df5e0820d149
tomunizua <t.omunizua@alustudent.com>
2023-07-28 10:55:52 -0700

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Function returns a list of all object attributes and methods
+'''
+
+
+def lookup(obj):
+    '''Returns all the methods and attributes
+    '''
+    return dir(obj)
