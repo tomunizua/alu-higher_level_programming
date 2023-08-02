62d7c4bc47cfe075657d4fb71d04bf25dfd5299f
tomunizua <t.omunizua@alustudent.com>
2023-08-02 07:07:45 -0700

    advanced

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
 -- A script that displays the max temperature of each state (ordered by State name)
-SELECT city, MAX(value) AS max_temp
+SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
