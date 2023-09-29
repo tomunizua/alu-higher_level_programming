0eeb9b11250a755e0341ffa3df84ec44487496dd
tomunizua <t.omunizua@alustudent.com>
2023-09-29 05:03:27 -0700

    7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
-sdffhhjj
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if (x) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
