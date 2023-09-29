663b06fb4ed0f51b0a1dae619b3a71e3502f18da
tomunizua <t.omunizua@alustudent.com>
2023-09-29 04:53:20 -0700

    5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const input = process.argv[2];
-if (isNaN(input)) {
+if (!isNaN(input)) {
   console.log('My number: ' + parseInt(input));
 } else {
   console.log('Not a number');
