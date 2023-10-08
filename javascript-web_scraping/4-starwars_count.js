9b6bc80a82565f492ef3b2560effc11636ad07dc
tomunizua <t.omunizua@alustudent.com>
2023-10-08 12:14:44 -0700

    4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    for (const filmIndex in films) {
-      const filmChars = films[filmIndex].characters;
-      for (const charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    const movieCount = data.results.length;
+    let finalResult = 0;
+    // const comString = 'https://swapi-api.hbtn.io/api/people/18/';
+    for (let i = 0; i < movieCount; i++) {
+      const numChars = data.results[i].characters.length;
+      for (let j = 0; j < numChars; j++) {
+        const comString0 = data.results[i].characters[j];
+        if (comString0.slice(-4, -1) === '/18') {
+          finalResult += 1;
         }
       }
     }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log(finalResult);
   }
-});`
+});
